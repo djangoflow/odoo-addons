2d83bd4da9e8ab88ee0037b5d0547f49a4985221
Yevhen Bondar <eugenij.bondar@gmail.com>
2022-11-16 09:35:11 +0200

    add tx creation for stripe_create_checkout_session and check_stripe_transaction_done endpoint

--- payment_stripe_xmlrpc/models/payment.py
+++ payment_stripe_xmlrpc/models/payment.py
@@ -16,15 +16,51 @@ class PaymentAcquirerStripeSession(models.Model):
         if order_id:
             order = self.env['sale.order'].search([('id', '=', order_id)], limit=1)
 
+            reference = order.name
+            acquirer = self
+            acquirer_id = self.id
+            currency_id = order.currency_id
+            partner_id = order.partner_id.id
+            invoice_id = None
+            amount = order.amount_total
+
+            ############
+            # Code from addons/payment/controllers/portal.py: WebsitePayment.transaction
+            reference_values = order_id and {'sale_order_ids': [(4, order_id)]} or {}
+            reference = self.env['payment.transaction']._compute_reference(values=reference_values, prefix=reference)
+
+            values = {
+                'acquirer_id': int(acquirer_id),
+                'reference': reference,
+                'amount': float(amount),
+                'currency_id': int(currency_id),
+                'partner_id': partner_id,
+                'type': 'form_save' if acquirer.save_token != 'none' and partner_id else 'form',
+            }
+
+            if order_id:
+                values['sale_order_ids'] = [(6, 0, [order_id])]
+            elif invoice_id:
+                values['invoice_ids'] = [(6, 0, [invoice_id])]
+
+            reference_values = order_id and {'sale_order_ids': [(4, order_id)]} or {}
+            reference_values.update(acquirer_id=int(acquirer_id))
+            values['reference'] = self.env['payment.transaction']._compute_reference(values=reference_values,
+                                                                                        prefix=reference)
+            tx = self.env['payment.transaction'].sudo().with_context(lang=None).create(values)
+            # End of WebsitePayment.transaction code
+            ##########
+
+
             stripe_session_data.update({
                 'line_items[][amount]': int(
                     order.amount_total if order.currency_id.name in INT_CURRENCIES else float_round(
                         order.amount_total * 100, 2)),
                 'line_items[][currency]': order.currency_id.name,
                 'line_items[][quantity]': 1,
-                'line_items[][name]': order.name,
-                'client_reference_id': order.name,
-                'payment_intent_data[description]': order.name,
+                'line_items[][name]': values['reference'],
+                'client_reference_id': values['reference'],
+                'payment_intent_data[description]': values['reference'],
                 'customer_email': order.partner_id.email,
             })
 
@@ -32,3 +68,15 @@ class PaymentAcquirerStripeSession(models.Model):
             stripe_session_data['payment_method_types[0]'] = 'card'
 
         return self.with_context(stripe_manual_payment=True)._create_stripe_session(stripe_session_data)
+
+
+class SaleOrderRPC(models.Model):
+    _inherit = 'sale.order'
+
+    def check_stripe_transaction_done(self):
+        self.ensure_one()
+
+        for tx in self.transaction_ids:
+            if tx.form_feedback({"reference": tx.reference}, "stripe"):
+                return True
+        return False
